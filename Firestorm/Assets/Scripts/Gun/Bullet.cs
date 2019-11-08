551366c24d5b41d55b271c18c54024020bf9559e
MrNiceGuy12312 <52978365+MrNiceGuy12312@users.noreply.github.com>
2019-11-08 14:50:50 -0500

    My Final Push

--- Firestorm/Assets/Scripts/Gun/Bullet.cs
+++ Firestorm/Assets/Scripts/Gun/Bullet.cs
@@ -8,10 +8,11 @@ public class Bullet : MonoBehaviour
     public int damage= 40;
     public Rigidbody2D rb;
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * Speed;
     }
     void OnTriggerEnter2D(Collider2D hitInfo)
